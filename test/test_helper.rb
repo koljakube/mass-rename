49477775ae0da643f8d02e8ca0fb71e65aeb31ca
Kolja Kube <kolja.kube@gmail.com>
2012-06-14 14:16:51 +0200

    added test helper

---
+++ test/test_helper.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/statik'
+
+class TestCase < MiniTest::Unit::TestCase
+  include MassRename
+  
+  class << self
+    alias_method :old_test, :test
+      
+    def test(name, &block)
+      name = "test_#{name.gsub(/[^\w ]/, '').gsub(' ', '_')}"
+      define_method name, &block
+    end
+    
+    def setup(&block)
+      define_method :setup, &block
+    end
+    
+    def teardown(&block)
+      define_method :teardown, &block
+    end
+  end
+end
